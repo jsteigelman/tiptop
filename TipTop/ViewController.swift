e26df15524b47169fe5ce3f504e342b080c88df7
Joey Steigelman <joey.steigelman@gmail.com>
2021-06-14 16:15:00 +0200

    Initial Commit

---
+++ TipTop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipTop
+//
+//  Created by Joey Steigelman on 6/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
